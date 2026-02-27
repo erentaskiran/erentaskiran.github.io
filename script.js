5ae0e7c5adc6dfd5ca7f5b3ea48c08579aea429b
Eren Taskiran <eren.tas3535@gmail.com>
2026-02-27 10:49:21 +0300

    week2

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    document.getElementById("loader").classList.add("hidden");
-    document.getElementById("content").classList.remove("hidden");
-  }, 2000);
-});
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// Sayfa yüklendiğinde konsola bir mesaj yazdırır.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Kişisel Portfolyo Sitesi Hazır!");
+});
